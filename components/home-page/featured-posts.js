d7dd628dc1f4e567ac0443d6549be59521cb2ee2
YiroYi <yiroyi@live.com>
2021-05-29 11:10:04 -0500

    Creating Post Grid component

--- components/home-page/featured-posts.js
+++ components/home-page/featured-posts.js
@@ -1,7 +1,10 @@
 import classes from './featured-posts.module.css';
 
 const FeaturedPosts = props => {
-  return;
+  return
+  <section className={classes.latest}>
+    <h2>Featured Posts</h2>
+  </section>
 }
 
 export default FeaturedPosts;
