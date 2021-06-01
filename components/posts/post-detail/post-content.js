df412fc6dc5794e57507e52b4f05bff1f206a0e6
YiroYi <yiroyi@live.com>
2021-05-31 21:54:35 -0500

    Complete Post Detail page

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,10 +1,20 @@
 import PostHeader from './post-header';
 
+const DUMMY_POST = {
+  slug: 'getting-starting-with-nextjs1',
+  title: 'Getting started with NextJS',
+  image: 'images-1.jpg',
+  date: '2021-05-06',
+  content: '# This is a first post'
+}
+
 const PostContent = () => {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+
   return(
     <article>
-      <PostHeader />
-      CONTENT
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
     </article>
   );
 }
