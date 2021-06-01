f675700dde51e48b400f4145ff622edf099da8a7
YiroYi <yiroyi@live.com>
2021-05-31 21:46:11 -0500

    Adding POSTHeader component

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,12 @@
+import PostHeader from './post-header';
+
+const PostContent = () => {
+  return(
+    <article>
+      <PostHeader />
+      CONTENT
+    </article>
+  );
+}
+
+export default PostContent;
