7edda3b08d159be107af940c8a4626f174838551
YiroYi <yiroyi@live.com>
2021-06-01 00:02:31 -0500

    Adding get staticProps to get all the posts

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,26 @@
-import Hero from '../components/home-page/hero';
-import FeaturedPosts from '../components/home-page/featured-posts';
-
 import { Fragment } from 'react';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-starting-with-nextjs1',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  },
-  {
-    slug: 'getting-starting-with-nextjs2',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  },
-  {
-    slug: 'getting-starting-with-nextjs3',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  },
-  {
-    slug: 'getting-starting-with-nextjs4',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  }
-];
+import Hero from '../components/home-page/hero';
+import FeaturedPosts from '../components/home-page/featured-posts';
+import { getFeaturedPosts } from '../lib/post-util';
 
-
-const HomePage = () => {
+const HomePage = props => {
   return(
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS}/>
+      <FeaturedPosts posts={props.posts}/>
     </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts
+    }
+  }
 }
 
 export default HomePage;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,38 +1,18 @@
 import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from '../../lib/post-util';
 
-const DUMMY_POSTS = [
-  {
-    slug: 'getting-starting-with-nextjs1',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  },
-  {
-    slug: 'getting-starting-with-nextjs2',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  },
-  {
-    slug: 'getting-starting-with-nextjs3',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
-  },
-  {
-    slug: 'getting-starting-with-nextjs4',
-    title: 'Getting started with NextJS',
-    image: 'images-1.jpg',
-    excerpt: 'This city is amazing for life, work & fun',
-    date: '2021-05-06',
+const AllPostsPage = props => {
+  return <AllPosts posts={props.posts}/>;
+}
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts
+    }
   }
-];
-
-const AllPostsPage = () => {
-  return <AllPosts posts={DUMMY_POSTS}/>;
 }
 
 export default AllPostsPage;
