b11d7308d90534c4815c7069486ad594ad846492
YiroYi <yiroyi@live.com>
2021-06-01 00:51:50 -0500

    Adding images with markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMardown from 'react-markdown';
+import Image from 'next/image';
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -7,10 +8,41 @@ const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`
 
+  const customRenderers = {
+    // img(image) {
+    //   return <Image
+    //             src={`/images/posts/${post.slug}/${image.src}`}
+    //             alt={image.alt}
+    //             width={600}
+    //             height={300}
+    //           />
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === 'img') {
+        const image = node.children[0];
+
+        return(
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>
+    }
+  }
+
   return(
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMardown>{post.content}</ReactMardown>
+      <ReactMardown components={customRenderers}>{post.content}</ReactMardown>
     </article>
   );
 }
