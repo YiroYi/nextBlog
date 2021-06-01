f11d8a2224b4c79c9c784c635b5e2aed40bc5c86
YiroYi <yiroyi@live.com>
2021-06-01 01:08:30 -0500

    Adding snippets

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,8 @@
 import ReactMardown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
 
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
@@ -28,6 +31,17 @@ const PostContent = (props) => {
       }
 
       return <p>{paragraph.children}</p>
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />);
     }
   }
 
