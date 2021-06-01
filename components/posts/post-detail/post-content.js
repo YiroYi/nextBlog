1ca7c7b2c7d3e9b8e8f9d9ba8ed34d39787d5090
YiroYi <yiroyi@live.com>
2021-05-31 23:14:56 -0500

    Installing ReactMarkdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,7 @@
+import ReactMardown from 'react-markdown';
+
 import PostHeader from './post-header';
+import classes from './post-content.module.css';
 
 const DUMMY_POST = {
   slug: 'getting-starting-with-nextjs1',
@@ -12,9 +15,9 @@ const PostContent = () => {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
 
   return(
-    <article>
+    <article className={classes.content}>
       <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      {DUMMY_POST.content}
+      <ReactMardown>{DUMMY_POST.content}</ReactMardown>
     </article>
   );
 }
